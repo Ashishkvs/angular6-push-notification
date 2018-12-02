999e705da6cdb57ec706fac13b3f869cdcdf4c9f
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2018-12-02 13:01:53 +0530

    intial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ToastrManager } from 'ng6-toastr-notifications';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'push-notification';
+  constructor(public toastr: ToastrManager) {
+    this.showToast();
+   }
+
+  showSuccess() {
+    console.log('Ashish');
+    this.toastr.successToastr('this is success toast', 'Success');
+  }
+
+  showError() {
+    this.toastr.errorToastr('This is error toast.', 'Oops!');
+  }
+
+  showWarning() {
+    this.toastr.warningToastr('This is warning toast.', 'Alert!');
+  }
+
+  showInfo() {
+    this.toastr.infoToastr('This is info toast.', 'Info');
+  }
+
+  showCustom() {
+    this.toastr.customToastr('Custom Toast', null, { enableHTML: true });
+  }
+
+  showToast(position: any = 'top-left') {
+    this.toastr.infoToastr('This is a toast.', 'Toast', { position: position });
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToastrModule } from 'ng6-toastr-notifications';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
